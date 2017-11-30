1b421b54258a7d2ec23bc9017318da4d2b8ac346
Tommy Huynh <Tom_T_Huynh@Outlook.com>
2017-11-29 23:34:26 -0600

    add php

--- assets/PHPfunctions/db_fns.php
+++ assets/PHPfunctions/db_fns.php
@@ -1,15 +1,17 @@
 <?php
 	//post-condition: when called on, this function will connect to the mySQL database.
 	function db_connect(){
-		$result = new mysqli('localhost', 'root', '021696', 'market');
+		$result = new mysqli('localhost', 'root', '07Loganoliver', 'market');
+//		$result = new mysqli('localhost', 'root', '07Loganoliver', 'market');
 		if(!$result)
 			throw new Exception('Could not connect to database server');
 		else
 			return $result;
 	}
+
 	function pdo_connection() {
-//		$pdo = new PDO('mysql:host=dbs.mysql.database.azure.com;dbname=market', 'root', '');
-		$pdo = new PDO('mysql:host=dbs.mysql.database.azure.com;dbname=market', 'smadmin@team4supermarket-dbs', 'cosC3380!');
+//		$pdo = new PDO('mysql:host=localhost;dbname=market', 'root', '07Loganoliver');
+		$pdo = new PDO('mysql:host=localhost;dbname=market', 'root', '07Loganoliver');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 		return $pdo;
---
+++ delete.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('assets/PHPfunctions/bookmark_fns.php');
+session_start();
+
+if(!(isset($_SESSION['valid_user'])) || !(isset($_SESSION['priviledge']))){
+    do_html_header_universal('form');
+    user_message('You are not logged in');
+    do_html_url('index.html', 'Login');
+    do_html_footer_universal(false, 'form');
+    exit;
+}
+
+if($_SESSION['priviledge'] != "employee"){
+    do_html_header_universal('form');
+    user_message('You do not have clearance for this page');
+    do_html_url('member.php', 'Go back');
+    do_html_footer_universal(false, 'form');
+    exit;
+}
+
+if ( isset($_GET['t']) && isset($_GET['i']) ) {
+    $table = $_GET['t'];
+    $pK = base64_decode($_GET['i']);
+
+    switch ( $table ) {
+        case 'employee':
+            $conn = pdo_connection();
+            $stmt = $conn->prepare('DELETE FROM employee WHERE `EName` = ?');
+            $stmt->execute(array($pK));
+            header('Location: /employee.php');
+            break;
+        case 'supplier':
+            $conn = pdo_connection();
+            $stmt = $conn->prepare('DELETE FROM supplier WHERE `CompanyID` = ?');
+            $stmt->execute(array($pK));
+            header('Location: /supplier.php');
+            break;
+        case 'product':
+            $conn = pdo_connection();
+            $stmt = $conn->prepare('DELETE FROM product WHERE `BarCode` = ?');
+            $stmt->execute(array($pK));
+            header('Location: /products.php');
+            break;
+    }
+}
