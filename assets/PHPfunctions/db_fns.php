12f79ca86432b0fa63c1fa3d1667df87aad1eeac
Tommy Huynh <Tom_T_Huynh@Outlook.com>
2017-11-30 00:20:46 -0600

    update db_fns

--- assets/PHPfunctions/db_fns.php
+++ assets/PHPfunctions/db_fns.php
@@ -1,7 +1,7 @@
 <?php
 	//post-condition: when called on, this function will connect to the mySQL database.
 	function db_connect(){
-		$result = new mysqli('dbs.mysql.database.azure.com', 'smadmin@team4supermarket-dbs', 'cosC3380!', 'market');
+		$result = new mysqli('team4supermarket-dbs.mysql.database.azure.com', 'smadmin@team4supermarket-dbs', 'cosC3380!', 'market');
 //		$result = new mysqli('localhost', 'root', '07Loganoliver', 'market');
 		if(!$result)
 			throw new Exception('Could not connect to database server');
@@ -11,7 +11,7 @@
 
 	function pdo_connection() {
 //		$pdo = new PDO('mysql:host=localhost;dbname=market', 'root', '07Loganoliver');
-		$pdo = new PDO('mysql:host=dbs.mysql.database.azure.com;dbname=market', 'smadmin@team4supermarket-dbs', 'cosC3380!');
+		$pdo = new PDO('mysql:host=team4supermarket-dbs.mysql.database.azure.com;dbname=market', 'smadmin@team4supermarket-dbs', 'cosC3380!');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 		return $pdo;
